0aa8dee75eac2b84761b27042234b1fdadb9bb50
ElgunQasimov <tu7Q2ECXM@code.edu.az>
2024-05-22 13:17:21 +0400

    21may task

---
+++ front-and/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/Header/index"
+import Section1 from "./components/Section1/index"
+import Section2 from "./components/Section2/index"
+import Section3 from "./components/Section3/index"
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Header/>
+    <Section1/>
+    <Section2/>
+    <Section3/>
+    </>
+  )
+}
+
+export default App
